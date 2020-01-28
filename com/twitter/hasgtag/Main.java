41f94739acb97e46c79ff1e3eb04f8bc5511ba9d
abhishekjainiitg <abhishekjainiitg@gmail.com>
2020-01-29 01:54:59 +0530

    first commit

---
+++ com/twitter/hasgtag/Main.java
@@ -0,0 +1,45 @@
+package com.twitter.hasgtag;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String fileName = "D:\\Programming\\Java\\3JavaInteractiveCourses\\twitterHastagExtractor\\src\\com\\twitter\\hasgtag\\data.txt";
+
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName)))
+        {
+
+            String currLine;
+            List<String> hashtags = new ArrayList<>();
+
+            while ((currLine = bufferedReader.readLine()) != null)
+            {
+                String[] words = currLine.split(" ");
+                for(String word: words) {
+                    if(word.contains("#") && word.indexOf("#") == 0 && !hashtags.contains(word)) {
+                        hashtags.add(word);
+                    }
+                }
+            }
+
+            hashtags.sort(String::compareTo);
+
+            for(String hashtag: hashtags) {
+                System.out.println(hashtag);
+            }
+
+            System.out.println(hashtags.size());
+
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+    }
+}
